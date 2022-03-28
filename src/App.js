0cd361907d3a1eb32382a24c782c50cbc150b335
Gregory Olive <gregoryolive@outlook.com>
2022-03-28 23:20:01 +0900

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './styles/App.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 const Instructions = (<p>画像をクリックしてポイントを獲得して、写真を2回クリックしたらゲームオーバー!</p>);
 
 const App = () => {
   return (
     <>
-    <Header/>
-    <main>
-      
-    </main>
+      <Header/>
+      <main>
+        
+      </main>
+      <Footer/>
     </>
   );
 }

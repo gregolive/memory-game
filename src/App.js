eb50733b9c24bf48f8b399dfc093bb6359d2075a
Gregory Olive <gregoryolive@outlook.com>
2022-03-29 21:10:55 +0900

    Add basic game functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './styles/App.css';
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import randomCharacters from './helpers/characters';
 import Header from './components/Header';
 import Card from './components/Card';
@@ -10,12 +10,23 @@ const App = () => {
   const [best, setBest] = useState(0);
   const [guessed, setGuessed] = useState([]);
 
+  const handleClick = (character) => {
+    if (guessed.includes(character)) {
+      setGuessed([]);
+      setScore(0);
+      return;
+    }
+    setGuessed(guessed.concat(character));
+    setScore(score + 1);
+    if (score >= best) { setBest(best + 1); }
+  };
+
   return (
     <>
       <Header score={score} best={best}/>
       <main>
         <div className='CardGrid'>
-          {randomCharacters().map((character) => <Card name={character.name} img={character.img}/>)}
+          {randomCharacters().map((character) => <Card key={character.id} character={character} handleClick={handleClick}/>)}
         </div>
         <Footer/>
       </main>
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,9 +2,9 @@ import '../styles/Card.css';
 
 const Card = (props) => {
   return (
-    <button className='Card'>
-      <img src={props.img} alt='' className='CardImg'/>
-      <h2>{props.name}</h2>
+    <button className='Card' onClick={() => props.handleClick(props.character.id)}>
+      <img src={props.character.img} alt='' className='CardImg'/>
+      <h2>{props.character.name}</h2>
     </button>
   );
 };
